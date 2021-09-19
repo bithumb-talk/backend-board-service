8f7bed53cc9d4278284c785a6308d2deaa9a1e4d
youminji <upi907@naver.com>
2021-09-20 05:20:05 +0900

    base

---
+++ src/test/java/com/bithumb/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bithumb.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
