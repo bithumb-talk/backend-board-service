491f71d0e1179083142aef35128987a3f488d3ca
youminji <upi907@naver.com>
2021-09-21 12:39:16 +0900

    feat: 게시판 전체 조회

---
+++ src/main/java/com/bithumb/board/controller/BoardAssembler.java
@@ -0,0 +1,45 @@
+package com.board.api.boardMs.board.controller;
+
+import com.bithumb.board.domain.Board;
+import com.bithumb.board.domain.BoardModel;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class BoardAssembler extends RepresentationModelAssemblerSupport<Board, BoardModel> {
+    public BoardAssembler(){
+        super(BoardController.class, BoardModel.class);
+    }
+    @Override
+    public BoardModel toModel(Board entity) {
+        BoardModel model = instantiateModel(entity);
+        model.setBoardNo(entity.getBoardNo());
+        model.setBoardTitle(entity.getBoardTitle());
+        model.setBoardCategory(entity.getBoardCategory());
+        model.setBoardContent(entity.getBoardContent());
+        model.setBoardViews(entity.getBoardViews());
+        model.setBoardRecommend(entity.getBoardRecommend());
+        model.setBoardCreatedDate(entity.getBoardCreatedDate());
+        model.setBoardModifyDate(entity.getBoardModifyDate());
+        model.add(linkTo(methodOn(BoardController.class).retrieveBoard(entity.getBoardNo())).withSelfRel());
+
+        return model;
+    }
+//    @Override
+//    public BoardModel toModel(Board entity) {
+//        return createModelWithId(entity.getBoardNo(), entity);
+//    }
+//    @Override
+//    public CollectionModel<BoardModel> toCollectionModel(Iterable<? extends Board> entities){
+//
+//        return super.toCollectionModel(entities.)
+//
+//                .add(linkTo(methodOn(BoardController.class).hateoasCollection()).withSelfRel())
+//                ;
+//    }
+
+
+}
---
+++ src/main/java/com/bithumb/board/service/ReplyService.java
@@ -0,0 +1,13 @@
+package com.board.api.boardMs.board.service;
+
+import com.bithumb.board.domain.Reply;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public interface ReplyService {
+    Reply save(Reply reply);
+    Optional<Reply> findById(long reply_no);
+    void deleteById(long reply_no);
+}
