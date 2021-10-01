539fc07ecfc94454a443fe3786a7245f5626cb2a
youminji <upi907@naver.com>
2021-10-02 00:43:37 +0900

    feat: eureka 설정

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM openjdk:17-ea-11-jdk-slim
 WORKDIR /server
-ARG JAR_FILE=./build/libs/*.jar
+ARG JAR_FILE=./build/libs/board-0.0.1-SNAPSHOT.jar
 ADD ${JAR_FILE} board-service.jar
-EXPOSE 8080
+EXPOSE 6000
 ENTRYPOINT ["java", "-jar", "board-service.jar"]
--- src/main/java/com/bithumb/board/BoardApplication.java
+++ src/main/java/com/bithumb/board/BoardApplication.java
@@ -2,7 +2,9 @@ package com.bithumb.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class BoardApplication {
 
