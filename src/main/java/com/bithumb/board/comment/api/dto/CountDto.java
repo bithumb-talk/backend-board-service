d21f2a0e2a646451f2d4e63c292b44df097d408d
youminji <upi907@naver.com>
2021-10-01 15:45:16 +0900

    refactor: 댓글 추천 API 수정

--- src/main/java/com/bithumb/board/comment/api/dto/CountDto.java
+++ src/main/java/com/bithumb/board/comment/api/dto/CountDto.java
@@ -2,12 +2,13 @@ package com.bithumb.board.comment.api.dto;
 
 import com.bithumb.board.board.domain.Board;
 import com.bithumb.board.comment.domain.Comment;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@Getter
 public class CountDto {
     private long commentRecommend;
-    public long getCommentRecommend(){
-        return this.commentRecommend;
-    }
     public CountDto(long commentRecommend) {
         this.commentRecommend = commentRecommend;
     }
