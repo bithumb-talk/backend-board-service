fc130add573fb13a9cad25b149cc536bccb9c977
youminji <upi907@naver.com>
2021-10-13 00:19:21 +0900

    test: apm서버

--- src/main/java/com/bithumb/board/board/api/BoardController.java
+++ src/main/java/com/bithumb/board/board/api/BoardController.java
@@ -65,7 +65,7 @@ public class BoardController {
     /* 게시글 추천 */
     @ApiOperation(value="게시글 좋아요", notes="게시글 좋아요 버튼" +
             "\n 게시글id(게시글 넘버), 현재 좋아요 갯수 응답")
-    @PostMapping("/boards/{board-no}/auth/recommend") //
+    @PostMapping("/boards/{board-no}/auth/recommend")
     public ResponseEntity countingBoardRecommend(
             @ApiParam(value = "boardNo", required = true, example = "1")
             @PathVariable(value="board-no") long boardNo,
